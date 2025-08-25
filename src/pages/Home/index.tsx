82acd1113cc903a43aa68c0de312e25adf7e0d7e
Yuri Romeu <yuriromeu2007@gmail.com>
2025-08-25 11:32:27 -0300

    finalização da home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,107 @@
+import { info } from 'console';
+import Banner from '../../components/Banner';
+import ProductsList from '../../components/ProductsList';
+import residente from '../../assets/images/resident.png';
+import diablo from '../../assets/images/diablo.png';
+import zelda from '../../assets/images/zelda.png';
+import starWars from '../../assets/images/star_wars.png';
+import streetFighter from '../../assets/images/streetFight.png';
+import Game from '../../models/Game';
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: residente,
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...'
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    title: '',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: residente,
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...'
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: residente,
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...'
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: residente,
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...'
+  }
+];
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    image: diablo,
+    title: 'Diablo IV',
+    infos: ['17/05'],
+    system: 'Windows'
+  },
+  {
+    id: 6,
+    category: 'Aventura',
+    description:
+      'Star Wars Jedi: Survivor  é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
+    infos: ['05/04'],
+    image: starWars,
+    title: 'Star Wars',
+    system: 'Windows'
+  },
+  {
+    id: 7,
+    category: 'Ação',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: residente,
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...'
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
+    infos: ['05/04'],
+    image: zelda,
+    title: 'The Legend of Zelda - TOK',
+    system: 'Switch'
+  }
+];
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <ProductsList games={promocoes} title="Promoções" background="gray" />
+      <ProductsList games={emBreve} title="Em Breve" background="black" />
+    </>
+  );
+};
+
+export default Home;
