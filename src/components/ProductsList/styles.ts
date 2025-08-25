318d0f46494b05f10c65d23101f5654a53b7224a
Yuri Romeu <yuriromeu2007@gmail.com>
2025-08-25 10:30:10 -0300

    estilização das seções e nos componetes de Card

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
+import { Props } from '.';
+import { Cores } from '../../styles';
+import { Card } from '../Product/styles';
 
-export const Container = styled.section`
+export const Container = styled.section<Omit<Props, 'title'>>`
   padding: 32px 0;
+  background-color: ${(props) =>
+    props.background === 'black' ? Cores.preta : Cores.cinza};
+
+  ${Card} {
+    background-color: ${(props) =>
+      props.background === 'black' ? Cores.cinza : Cores.preta};
+  }
 `;
 
 export const List = styled.ul`
